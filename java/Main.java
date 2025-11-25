817fc8bfd2e09dbf3811ce2b4a23328bfe79ec59
amartii <amartinez.tellez@gmail.com>
2025-11-25 16:10:02 +0100

    primer programa en java

---
+++ java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola, Java!");
+    }
+}
+
+
+// compilar el programa de java con "javac Main.java"
+//ejecutar con "java Main"
